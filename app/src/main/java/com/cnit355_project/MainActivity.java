de0fe5c9e39af73722de52750eaf9019ec42b28d
Trevor Vannatta <tvannattak@gmail.com>
2023-11-07 13:45:35 -0500

    initial commit

--- app/src/main/java/com/cnit355_project/MainActivity.java
+++ app/src/main/java/com/cnit355_project/MainActivity.java
@@ -2,6 +2,7 @@ package com.cnit355_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClick (View v)
+    {
+        if (v.getId() == R.id.button)
+        {
+
+        }
+
+        if (v.getId() == R.id.button2)
+        {
+
+        }
+
+        if (v.getId() == R.id.button3)
+        {
+
+        }
     }
 }
