1a96ad9b23c96085b357b919e92ac36a9adc633c
Trevor Vannatta <tvannattak@gmail.com>
2023-12-10 14:25:43 -0500

    initial commit

--- app/src/main/java/com/cnit355_project/FragmentB3.java
+++ app/src/main/java/com/cnit355_project/FragmentB3.java
@@ -20,21 +20,8 @@ import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentB3#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FragmentB3 extends Fragment implements View.OnClickListener {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
 
 
@@ -52,31 +39,11 @@ public class FragmentB3 extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentB3.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentB3 newInstance(String param1, String param2) {
-        FragmentB3 fragment = new FragmentB3();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
 
