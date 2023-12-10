d70dd1132bebf63a61734ee383f411565e2040ad
ngebresl <112847382+ngebresl@users.noreply.github.com>
2023-12-09 23:31:37 -0500

    Update FragmentA2.java

--- app/src/main/java/com/cnit355_project/FragmentA2.java
+++ app/src/main/java/com/cnit355_project/FragmentA2.java
@@ -1,14 +1,24 @@
 package com.cnit355_project;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +35,15 @@ public class FragmentA2 extends Fragment implements View.OnClickListener {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    SqlDBHelper myHelper;
+    SQLiteDatabase db;
+    Cursor c;
+    CheckBox checkBox1, checkBox2, checkBox3, checkBox4;
+    Button buttonBack, buttonRemove,buttonInsert, buttonUpdate, buttonWorkoutPlan;
+    View view;
+    TextView error;
+    EditText et1_set, et1_rep, et2_set, et2_rep, et3_set,  et3_rep, et4_set, et4_rep;
 
     public FragmentA2() {
         // Required empty public constructor
@@ -58,9 +77,7 @@ public class FragmentA2 extends Fragment implements View.OnClickListener {
     }
 
 
-    CheckBox checkBox1, checkBox2, checkBox3, checkBox4;
-    Button buttonBack, buttonCreate, buttonAll, buttonNone;
-    View view;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -68,15 +85,30 @@ public class FragmentA2 extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_a2, container, false);
 
+        myHelper = new SqlDBHelper(getActivity());
+
         // Replace these IDs with the actual IDs used in your XML layout
         checkBox1 = view.findViewById(R.id.checkBox1);
         checkBox2 = view.findViewById(R.id.checkBox2);
         checkBox3 = view.findViewById(R.id.checkBox3);
         checkBox4 = view.findViewById(R.id.checkBox4);
         buttonBack = view.findViewById(R.id.buttonBack1);
-        buttonCreate = view.findViewById(R.id.buttonCreate1);
-        buttonAll = view.findViewById(R.id.buttonAll1);
-        buttonNone = view.findViewById(R.id.buttonNone1);
+        buttonRemove = view.findViewById(R.id.buttonRemove);
+        buttonInsert = view.findViewById(R.id.buttonInsert);
+        buttonWorkoutPlan=view.findViewById(R.id.buttonWorkoutPlan);
+        buttonUpdate  = view.findViewById(R.id.buttonUpdate);
+        et1_set= view.findViewById(R.id.editText);
+
+        et1_rep= view.findViewById(R.id.editText2);
+        et2_set= view.findViewById(R.id.editText3);
+        et2_rep= view.findViewById(R.id.editText4);
+
+        et3_set= view.findViewById(R.id.editText5);
+        et3_rep= view.findViewById(R.id.editText6);
+        et4_set=view.findViewById(R.id.editText7);
+        et4_rep=view.findViewById(R.id.editText8);
+
+        error = view.findViewById(R.id.textError);
 
         // Set click listeners for the checkboxes
         checkBox1.setOnClickListener(this);
@@ -85,89 +117,174 @@ public class FragmentA2 extends Fragment implements View.OnClickListener {
         checkBox4.setOnClickListener(this);
 
         buttonBack.setOnClickListener(this);
-        buttonAll.setOnClickListener(this);
-        buttonNone.setOnClickListener(this);
+        buttonInsert.setOnClickListener(this);
+        buttonUpdate.setOnClickListener(this);
+        buttonRemove.setOnClickListener(this);
+        buttonWorkoutPlan.setOnClickListener(this);
 
 
+        error = view.findViewById(R.id.textError);
+        ImageView imageView1= (ImageView) view.findViewById(R.id.imageView1);
+        ImageView imageView2= (ImageView) view.findViewById(R.id.imageView2);
+        ImageView imageView3= (ImageView) view.findViewById(R.id.imageView3);
+        ImageView imageView4= (ImageView) view.findViewById(R.id.imageView4);
+
+        Glide.with(getActivity()).load(R.drawable.bench_press).into(imageView1);
+        Glide.with(getActivity()).load(R.drawable.shoulderpress).into(imageView2);
+        Glide.with(getActivity()).load(R.drawable.chest_dip).into(imageView3);
+        Glide.with(getActivity()).load(R.drawable.lat_raise).into(imageView4);
 
 
         return view;
     }
 
 
-    private void handleCheckboxClick(boolean isChecked) {
-        // Handle the checkbox click event
-        if (isChecked) {
-            // The checkbox is checked, implement your logic here
-        } else {
-            // The checkbox is unchecked, implement your logic here
-        }
-    }
 
     @Override
-    public void onClick(View v)
-    {
-
-        if (v.getId() == R.id.checkBox1)
-        {
-            handleCheckboxClick(checkBox1.isChecked());
-        }
-        else if (v.getId() == R.id.checkBox2)
-        {
-            handleCheckboxClick(checkBox1.isChecked());
-        }
-        else if (v.getId() == R.id.checkBox3)
-        {
-            handleCheckboxClick(checkBox1.isChecked());
-        }
-        else if (v.getId() == R.id.checkBox4)
-        {
-            handleCheckboxClick(checkBox1.isChecked());
-        }
-        else if (v.getId() == R.id.buttonBack1)
-        {
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonBack1) {
             getActivity().getSupportFragmentManager().popBackStackImmediate();
         }
-        else if (v.getId() == R.id.buttonCreate1)
-        {
 
+        // TODO: Replace the "create" button with a "remove" button since the database gets created automatically. Remove button will remove exercises from workoutplan.
+        else if (v.getId() == R.id.buttonRemove) {
+            if (checkBox1.isChecked()) {
+                myHelper.deleteData("Bench Press");
+                Log.i("FragA2", "BenchPress Deleted");
+            } else {
+                Log.i("FragA2", "BenchPress Doesn't Exist");
+
+            }
+            if (checkBox2.isChecked()) {
+                myHelper.deleteData("Shoulder Press");
+                Log.i("FragA2", "Shoulder Press Deleted");
+
+            } else {
+                Log.i("FragA2", "Shoulder Press Doesn't Exist");
+
+            }
+            if (checkBox3.isChecked()) {
+                myHelper.deleteData("Shoulder Dip");
+            } else {
+                Log.i("FragA2", "Shoulder Dip Doesn't Exist");
+
+            }
+            if (checkBox4.isChecked()) {
+                myHelper.deleteData("Lateral Raises");
+            } else {
+                Log.i("FragA2", "Lateral Raises Doesn't Exist");
+
+            }
+            //TODO: Repeat the above logic for the other 3 checkboxes.
+
+            Toast.makeText(getActivity(), "Removed selections from Workout Plan.", Toast.LENGTH_SHORT).show();
         }
-        else if (v.getId() == R.id.buttonNone1)
-        {
-            LinearLayout imageContainer = view.findViewById(R.id.imageContainer);
 
-            for (int i = 0; i < imageContainer.getChildCount(); i++) {
-                View child = imageContainer.getChildAt(i);
-                if (child instanceof LinearLayout) {
-                    LinearLayout linearLayout = (LinearLayout) child;
-                    for (int j = 0; j < linearLayout.getChildCount(); j++) {
-                        View innerChild = linearLayout.getChildAt(j);
-                        if (innerChild instanceof CheckBox) {
-                            CheckBox checkBox = (CheckBox) innerChild;
-                            checkBox.setChecked(false);
-                        }
-                    }
+
+        else if (v.getId() == R.id.buttonInsert)
+        {
+            if (checkBox1.isChecked()) {
+
+                if (checkIfAlreadyAdded("Bench Press")) {
+                    Log.i("FragA2", "BenchPress Already Added");
+
+                    error.append("\t\t\nBench Press already added to Workout Plan. ");
+                } else
+                    myHelper.insertData("Bench Press", et1_set.getText().toString(), et1_rep.getText().toString(), "A2");
+                Log.i("FragA2", "BenchPress inserted");
+                c.close();
+            }
+            else if (checkBox2.isChecked()) {
+                if (checkIfAlreadyAdded("Shoulder Press")) {
+                    Log.i("FragA2", "Shoulder Press Already Added");
+
+                    error.append("\t\t\nShoulder Press already added to Workout Plan. ");
+                } else {
+                    myHelper.insertData("Shoulder Press", et2_set.getText().toString(), et2_rep.getText().toString(), "A2");
+                    Log.i("FragA2", "Shoulder Press inserted");
+                    c.close();
+                }
+            }
+
+            else if (checkBox3.isChecked()) {
+
+                if (checkIfAlreadyAdded("Shoulder Dip")) {
+                    Log.i("FragA2", "Shoulder Dip Already Added");
+
+                    error.append("\t\t\nShoulder Dip already added to Workout Plan. ");
+                } else {
+                    myHelper.insertData("Shoulder Dip", et3_set.getText().toString(), et3_rep.getText().toString(), "A2");
+                    Log.i("FragA2", "Shoulder Dip inserted");
+                    c.close();
+                }
+
+            }
+            else if (checkBox4.isChecked()) {
+
+                if (checkIfAlreadyAdded("Lateral Raises")) {
+                    Log.i("FragA2", "Lateral Raises Already Added");
+
+                    error.append("\t\t\nLateral Raises already added to Workout Plan. ");
+                } else {
+                    myHelper.insertData("Lateral Raises", et4_set.getText().toString(), et4_rep.getText().toString(), "A2");
+                    Log.i("FragA2", "Lateral Raises");
+                    c.close();
                 }
             }
         }
-        else if (v.getId() == R.id.buttonAll1)
-        {
-            LinearLayout imageContainer = view.findViewById(R.id.imageContainer);
+        //OnUpdate
+        else if (v.getId() == R.id.buttonUpdate) {
+            if (checkBox1.isChecked()) {
 
-            for (int i = 0; i < imageContainer.getChildCount(); i++) {
-                View child = imageContainer.getChildAt(i);
-                if (child instanceof LinearLayout) {
-                    LinearLayout linearLayout = (LinearLayout) child;
-                    for (int j = 0; j < linearLayout.getChildCount(); j++) {
-                        View innerChild = linearLayout.getChildAt(j);
-                        if (innerChild instanceof CheckBox) {
-                            CheckBox checkBox = (CheckBox) innerChild;
-                            checkBox.setChecked(true);
-                        }
+                if (checkIfAlreadyAdded("Bench Press")) {
+                    if (et1_set.getText() != null || et1_rep.getText() != null) {
+                        myHelper.updateData("Bench Press", et1_set.getText().toString(), et1_rep.getText().toString());
                     }
                 }
             }
+            else if (checkBox2.isChecked()) {
+
+                if (checkIfAlreadyAdded("Shoulder Press")) {
+                    if (et2_set.getText() != null || et2_rep.getText() != null) {
+                        myHelper.updateData("Shoulder Press",  et2_set.getText().toString(), et2_rep.getText().toString());
+                    }
+
+                }
+            } else if (checkBox3.isChecked()) {
+
+                if (checkIfAlreadyAdded("Shoulder Dip")) {
+                    if (et3_set.getText() != null || et3_rep.getText() != null) {
+                        myHelper.updateData("Shoulder Dip",  et3_set.getText().toString(), et3_rep.getText().toString());
+                    }
+                }
+            }
+            else if (checkBox4.isChecked()) {
+
+                if (checkIfAlreadyAdded("Lateral Raises")) {
+                    if (et4_set.getText() != null || et4_rep.getText() != null) {
+                        myHelper.updateData("Lateral Raises",  et4_set.getText().toString(), et4_rep.getText().toString());
+                    }
+                }
+            }
+
+
         }
 
+        else if (v.getId() == R.id.buttonWorkoutPlan) {
+
+            Intent mIntent = new Intent(v.getContext(), WorkoutPlanActivity.class);
+            startActivity(mIntent);
+            Log.i("Log", "Intent to WorkoutPlan Started");
+        }
+    }
+    public boolean checkIfAlreadyAdded(String name) {
+        db = myHelper.getReadableDatabase();
+        c = db.rawQuery("SELECT eName FROM workoutplan WHERE eName =?", new String[]{name});
+
+        if (c.getCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
